24bc87834fac5c7dab41e8019587a7b03e00c138
Esther Xuanpei Ouyang <xuouyang@ucsd.edu>
2016-04-18 15:27:48 -0700

    Update KNN.m

--- KNN.m
+++ KNN.m
@@ -1,3 +1,13 @@
+% Implement k-th nearest neighbors classifier
+% 
+% Input: X: a matrix for training set features 
+%        Y_NUM: a vector raining set class labels
+%        toTest: a vector for testing set features
+%        kVal: specify the k value in k-th nearest neighbors
+%
+% Return: result: a vector for predicted class labels given features in toTest
+% 
+% Author: Xuanpei Ouyang
 function[result] = KNN(X,Y_NUM,toTest,kVal)
     result = zeros(size(toTest,1),1);
 
